88f1be5f993f983a41959ffbf7786a4e515960dd
Highmaster <nzechideraann@gmail.com>
2024-07-11 22:20:17 +0100

    DataLoader class

---
+++ src/main/java/dev/coded/content_calender/config/DataLoader.java
@@ -0,0 +1,13 @@
+package dev.coded.content_calender.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DataLoader implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello 👋🏽👋🏽👋🏽");
+    }
+}
