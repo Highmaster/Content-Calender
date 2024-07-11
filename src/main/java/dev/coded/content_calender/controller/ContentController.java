6cae55c88e64e0b9d8f7c79e8243dfab7e36c85d
Highmaster <nzechideraann@gmail.com>
2024-07-11 03:49:00 +0100

    made changes to the Content-Controller class

--- src/main/java/dev/coded/content_calender/controller/ContentController.java
+++ src/main/java/dev/coded/content_calender/controller/ContentController.java
@@ -1,7 +1,8 @@
 package dev.coded.content_calender.controller;
 
 import dev.coded.content_calender.model.Content;
-import dev.coded.content_calender.repository.ContentCollectionRepository;
+import dev.coded.content_calender.model.Status;
+import dev.coded.content_calender.repository.ContentRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,11 @@ import java.util.List;
 @CrossOrigin()
 public class ContentController {
 
-    private final ContentCollectionRepository repository;
+    private final ContentRepository repository;
 
-    public ContentController(ContentCollectionRepository repository) {
+
+
+    public ContentController(ContentRepository repository) {
         this.repository = repository;
     }
 
@@ -50,8 +53,18 @@ public class ContentController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void delete(Integer id){
-        repository.delete(id);
+    public void delete(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
+
+    @GetMapping("/filter/{keyword}")
+    public  List<Content> findByTitle(@PathVariable String keyword) {
+        return repository.findAllByTitleContains(keyword);
+    }
+
+    @GetMapping("/filter/status/{status}")
+    public List<Content> findByStatus(@PathVariable Status status) {
+        return  repository.listByStatus(status);
     }
 
 }
